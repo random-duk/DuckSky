40d81f8b8fbbdae8b49df8432efcc277884dd021
Geoffrey Westhoff <hello@auxim.dev>
2025-04-23 22:23:45 +0200

    feat: main bot

---
+++ DuckSky/Program.cs
@@ -0,0 +1,26 @@
+﻿using dotenv.net;
+using idunno.Bluesky;
+using RandomDukNET;
+
+DotEnv.Load(options: new DotEnvOptions(probeForEnv: true, envFilePaths: [".env"]));
+
+var username = Environment.GetEnvironmentVariable("BSKY_USERNAME") ?? string.Empty;
+var password = Environment.GetEnvironmentVariable("BSKY_PASSWORD") ?? string.Empty;
+var pds = new Uri(Environment.GetEnvironmentVariable("BSKY_PDS") ?? "https://bsky.social");
+
+// Credits to SoNearSonar for the RandomDukNET library
+var dukManager = new RandomDukManager();
+
+BlueskyAgent agent = new ();
+
+var loginResult = await agent.Login(username, password, service: pds );
+
+if (loginResult.Succeeded)
+{
+    var duckPic = await dukManager.GetRandomImage();   
+    var imageUploadResult = await agent.UploadImage(duckPic, "image/jpg", "A picture of a duck", null);
+    if (imageUploadResult.Succeeded)
+    {
+        var post = await agent.Post("\u200b", imageUploadResult.Result, null, null);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+# BlueSky Duck Bot
+<hr>
+
+## Description
+
+<hr>
+
+Because Ducks fly in the Blue Sky
+
+Main instance [@random-d.uk](https://bsky.app/profile/random-d.uk)
+
+
+## Credits
+<hr>
+
+[SoNearSonar's RandomDuk-NET package](https://github.com/SoNearSonar/RandomDuk-NET)
+
+[dotenv.net](https://github.com/bolorundurowb/dotenv.net) 
+
+[idunno.Bluesky](https://github.com/blowdart/idunno.Bluesky)
